19ae8eb58b27ff7eaf0e2e1289e3c581a30ff4f5
何平 <heping@hepingdeMacBook-Pro.local>
2022-07-16 21:38:47 +0800

    t

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package moody
 
+import "fmt"
+
 type alive interface {
 	walk()
 }
@@ -9,5 +11,13 @@ type People struct {
 }
 
 func (p People) walk() {
+
+}
+
+func main() {
+	i := string("2222")
+	var a int
+	a = 8
+	fmt.Println(a, i)
 
 }
